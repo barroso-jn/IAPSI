cc34c0a958fc18b771e7d2daff379c1e147e82bd
barroso-jn <92605854+barroso-jn@users.noreply.github.com>
2022-01-31 22:18:45 +0000

    Add files via upload
    Filtro a funcionar/ Pesquisa

--- PWC_V3/script.js
+++ PWC_V3/script.js
@@ -1,11 +1,4 @@
-// SALVAR VALOR DA PESQUISA AO CLICAR NO BOTÃO DE PESQUISAR
 
-function save(){
-    var resultado = document.getElementById('pesquisa').value;
-    var valuePesquisa2 = resultado.toLowerCase();
-    sessionStorage.setItem("resultado", valuePesquisa2);
-    location.href = "./resultado.html";
-    }
 
 // BUSCAR O RESULTADO DA PESQUISA EM FORMATO DE JSON E CONSTRUIR TABELA
 
@@ -31,35 +24,5 @@ $.ajax({
                 
             }  
         })
-      
-
-// FINALIZAR PESQUISA COM TECLA "ENTER"
-
-var input = document.getElementById("pesquisa")
-
-input.addEventListener("keyup", function(event) {
-            if (event.keyCode === 13) {
-              event.preventDefault();
-              document.getElementById("myBtn").click();
-            }
-        })
-
-
-//FUNÇÃO DE FILTRAR PELO NOME
-
-
-function search_coin() { 
     
-    let input = document.getElementById('searchname').value 
-    input=input.toLowerCase(); 
-    console.log(input)
-    let x = document.getElementsByClassName('cryptocurrencies'); 
-    for (i = 0; i < x.length; i++) { 
-        if (!x[i].innerHTML.toLowerCase().includes(input)) { 
-            x[i].style.display="none";
-             }
-        else { 
-            x[i].style.display="name"; 
-            input=""
-            } }
-        }
+
--- PWC_V3/script2.js
+++ PWC_V3/script2.js
@@ -22,9 +22,49 @@ $.ajax({
         })
 
 
-        function favoritos(){
-            var favoritos= document.getElementsByTagName('tr').value
-            console.log(favoritos)
+function favoritos(){
+    var favoritos= document.getElementsByTagName('tr').value
+    console.log(favoritos)
+    }
+
+
+// FINALIZAR PESQUISA COM TECLA "ENTER"
+
+var input = document.getElementById("pesquisa")
+
+input.addEventListener("keyup", function(event) {
+            if (event.keyCode === 13) {
+              event.preventDefault();
+              document.getElementById("myBtn").click();
+              
+            }
+        })
+
+    
+//FUNÇÃO DE FILTRAR PELO NOME
+
+
+function search_coin() { 
+    
+    let input = document.getElementById('searchname').value 
+    input=input.toLowerCase(); 
+    console.log(input)
+    let x = document.getElementsByClassName('cryptocurrencies'); 
+    for (i = 0; i < x.length; i++) { 
+        if (!x[i].innerHTML.toLowerCase().includes(input)) { 
+            x[i].style.display="none";
+             }
+        else { 
+            x[i].style.display="name"; 
+            } }
         }
 
-      
+// SALVAR VALOR DA PESQUISA AO CLICAR NO BOTÃO DE PESQUISAR
+
+function save(){
+    var resultado = document.getElementById('pesquisa').value;
+    var valuePesquisa2 = resultado.toLowerCase();
+    sessionStorage.setItem("resultado", valuePesquisa2);
+    location.href = "./resultado.html";
+ 
+    }
